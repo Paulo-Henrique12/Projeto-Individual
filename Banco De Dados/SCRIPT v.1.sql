ee10a21fb54379734c14c0465fdd250377af5d94
Paulo-Henrique12 <paulo.bambino@sptech.school>
2025-11-25 02:45:06 -0300

    Mudança nos campos do banco de dados

--- Banco De Dados/SCRIPT v.1.sql
+++ Banco De Dados/SCRIPT v.1.sql
@@ -4,16 +4,15 @@ USE GigantesDeAco;
 
 CREATE TABLE pessoa(
 idPessoa INT PRIMARY KEY AUTO_INCREMENT,
+nome VARCHAR(45),
 email VARCHAR(45) UNIQUE,
-senha VARCHAR(45),
-nome VARCHAR(45)
+senha VARCHAR(45)
 );
 
 
 CREATE TABLE quiz(
 idQuiz INT PRIMARY KEY AUTO_INCREMENT,
-avaliacao INT,
-tentativas INT
+descricao VARCHAR(100)
 );
 
 
@@ -22,11 +21,19 @@ CREATE TABLE resultados(
 fkPessoa INT,
 fkQuiz INT,
 idResultados INT,
+avaliacao INT,
 qtdAcertos INT,
 qtdErros INT,
+CONSTRAINT fkPessoaResultados
+	FOREIGN KEY(fkPessoa)
+		REFERENCES pessoa(idPessoa),
+CONSTRAINT fkQuizResultados
+	FOREIGN KEY (fkQuiz)
+		REFERENCES quiz(idQuiz),
 CONSTRAINT pkComposta
 	PRIMARY KEY (fkPessoa, fkQuiz, idResultados)
 );
 
-DROP DATABASE GigantesDeAco;
+
+
 
